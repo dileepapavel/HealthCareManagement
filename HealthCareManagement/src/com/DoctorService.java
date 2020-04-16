62a8b6037750e5cd2ec4392b17e333406ee0b1ae
pavel98 <51260268+dileepapavel@users.noreply.github.com>
2020-04-16 20:21:29 +0530

    Read CRUD was done

--- HealthCareManagement/src/com/DoctorService.java
+++ HealthCareManagement/src/com/DoctorService.java
@@ -1,5 +1,24 @@
 package com;
 
-public class DoctorService {
+import model.Doctor;
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+@Path("/Doctor")
 
+public class DoctorService {
+	Doctor doctorObj = new Doctor();
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readDoctor()
+	{
+	return doctorObj.readDoctor();
+	}
 }
